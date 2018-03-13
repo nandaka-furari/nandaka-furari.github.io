80fcf2142981bea317c4a80117b937e0c4af616d
ナンダカフラリ <nandaka.furari@gmail.com>
2018-03-13 18:17:42 +0900

    filesをもともとあった別のリポジトリで再び管理する

--- files/_labo/dirasfile/simplatabelo.d/sample.simplatabelo/sample.groovy
+++
@@ -1,2 +0,0 @@
-import org.w3c.dom.*
-
--- files/js/system/clean.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const rimraf = require('rimraf');
-fs.unlink('.dest/test.html');
-//rimraf('.dest',(e) => {console.log(e);});
-fs.access('.dest',fs.constants.R_OK | fs.constants.W_OK,(err) => {
-	console.log(err);
-})
