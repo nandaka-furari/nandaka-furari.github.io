91f5498a0ad08322f7fce0d8e3c4038b8f3377b0
ナンダカフラリ <nandaka.furari@gmail.com>
2020-06-19 23:53:48 +0900

    サイトの調整

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Get all "navbar-burger" elements
-	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-	// Check if there are any navbar burgers
-	if ($navbarBurgers.length > 0) {
-  
-	  // Add a click event on each of them
-	  $navbarBurgers.forEach( el => {
-		el.addEventListener('click', () => {
-  
-		  // Get the target from the "data-target" attribute
-		  const target = el.dataset.target;
-		  const $target = document.getElementById(target);
-  
-		  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-		  el.classList.toggle('is-active');
-		  $target.classList.toggle('is-active');
-  
-		});
-	  });
-	}
-  
+window.addEventListener('load', (event) => {
+	const header = new Vue({
+		el:"#navi",
+		data: {
+			ha0: false,
+			ha1: false,
+			ha2: false
+		},
+		methods: {
+			toggle: function(ha) {
+				if(ha=='ha0') {
+					this.ha0=!this.ha0;
+				} else if(ha=='ha1') {
+					this.ha1=!this.ha1;
+				} else if(ha=='ha2') {
+					this.ha2=!this.ha2;
+				}
+			}
+		}
+	})
   });
